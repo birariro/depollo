45abe691287257e5961fff2ffc7600e6a46d4ebe
JHLee <vps32@naver.com>
2023-05-17 17:14:28 +0900

    feat: project init

---
+++ src/main/java/SearchAction.java
@@ -0,0 +1,40 @@
+import java.util.Optional;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.lang.Language;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+
+public class SearchAction extends AnAction {
+
+	@Override
+	public void actionPerformed(@NotNull AnActionEvent e) {
+		Optional<PsiFile> psiFile = Optional.ofNullable(e.getData(LangDataKeys.PSI_FILE));
+		String languageTag = psiFile
+			.map(PsiFile::getLanguage)
+			.map(Language::getDisplayName)
+			.map(String::toLowerCase)
+			.map(lang -> "[" + lang + "]")
+			.orElse("");
+
+		Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+		CaretModel caretModel = editor.getCaretModel();
+		String selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+		BrowserUtil.browse("https://stackoverflow.com/search?q=" + languageTag + selectedText);
+	}
+
+	@Override
+	public void update(AnActionEvent e) {
+		Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+		CaretModel caretModel = editor.getCaretModel();
+		e.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
+	}
+}
