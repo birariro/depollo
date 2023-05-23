dea35de970fa618b3d80623517fe2d871fce192f
JHLee <vps32@naver.com>
2023-05-23 10:12:42 +0900

    feat: toolwindow 데이터 연결

--- src/main/java/CodeReviewAction.java
+++ src/main/java/CodeReviewAction.java
@@ -1,5 +1,6 @@
 
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 
@@ -15,10 +16,12 @@ import com.intellij.ui.content.ContentFactory;
 
 public class CodeReviewAction extends AnAction {
 
+	private static List<String> questions = new ArrayList<>();
 	@Override
 	public void actionPerformed(AnActionEvent e) {
 		// 드래그한 텍스트 얻기
 		String selectedText = e.getDataContext().getData(CommonDataKeys.EDITOR).getSelectionModel().getSelectedText();
+		questions.add(selectedText);
 
 		// toolWindow 얻기
 		Project project = e.getProject();
@@ -29,20 +32,33 @@ public class CodeReviewAction extends AnAction {
 		if (toolWindow != null) {
 			toolWindow.activate(null);
 			toolWindow.getContentManager().removeAllContents(true);
-			toolWindow.getContentManager().addContent(createContent(selectedText));
+			toolWindow.getContentManager().addContent(createContent(questions));
 		}
 	}
 
 
-	private Content createContent(String text) {
-		JTextPane textPane = new JTextPane();
-		textPane.setText(text);
+	private Content createContent(List<String> texts) {
+		JPanel mainPanel = new JPanel();
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-		JScrollPane scrollPane = new JScrollPane(textPane);
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.add(scrollPane, BorderLayout.CENTER);
+		for (String text : texts) {
+			JEditorPane textPane = createBubblePane(text);
+			panel.add(textPane);
+			panel.add(Box.createVerticalStrut(10)); // 간격 추가
+		}
+
+
+		mainPanel.add(panel);
 
 		ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-		return contentFactory.createContent(panel, null, false);
+		return contentFactory.createContent(mainPanel, null, false);
+	}
+
+	private JEditorPane createBubblePane(String text) {
+		JEditorPane textPane = new JEditorPane();
+		textPane.setContentType("text/html");
+		textPane.setText("<html><body><div style='padding: 5px; border: 1px solid black; width: 100%; background-color: #F0F0F0;'>" + text + "</div></body></html>");
+		return textPane;
 	}
 }
